9491d891f98094a8c5b228ceb50f94d9b5ddb449
Joshua C. Parsell <joshua_parsell@hotmail.com>
2020-07-13 11:09:40 -0400

    download, unzip, read, and filter on dates

--- plot1.R
+++ plot1.R
@@ -2,5 +2,11 @@
 # author:  Joshua Parsell
 
 # This R script reads energy monitoring data from 1-2 Feb 2007 and creats plot1.png
+library(dplyr)
 
+# Download the data
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip","power_cons.zip")
+
+# Unzip the file.
+unzip("power_cons.zip")
 
